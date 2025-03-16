6004c5cf653020e59e1e05fbbe41a764c12a2ddf
dhilip004 <dhilip0040@outlook.com>
2025-03-16 19:24:36 +0530

    Create Post and work

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,29 @@
+
+
+<html>   
+<head>
+    <title>Posts</title>
+</head>
+<body>
+    <h1>Posts</h1>
+    <li class="text-3xl font-bold underline">  <a href="/posts/1">Post 1</a>
+    <p>Author : Test</p>
+    
+     </li>
+
+    <li class="text-3xl font-bold underline"> <a href="/posts/2">Post 2</a> </li>
+
+     <p>Author : Test </p>
+
+</body>
+</html>
+
+    
+
+
+
+
+
+
+
+
